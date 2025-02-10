0dda730201a0dbbcdb79b83166de7c2d6e2aa5a9
Florent.Bourgeois <florent.bourgeois@uha.fr>
2025-02-10 14:40:20 +0100

    patch usageRepo en usagerRepo

--- src/main/java/fr/uha/serfa/lpdaoo25/BiblioTook/controller/UsagerController.java
+++ src/main/java/fr/uha/serfa/lpdaoo25/BiblioTook/controller/UsagerController.java
@@ -1,24 +1,22 @@
 package fr.uha.serfa.lpdaoo25.BiblioTook.controller;
 
 
-import fr.uha.serfa.lpdaoo25.BiblioTook.dao.UsageRepository;
+import fr.uha.serfa.lpdaoo25.BiblioTook.dao.UsagerRepository;
 import fr.uha.serfa.lpdaoo25.BiblioTook.model.Usager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/bibliotook")
 public class UsagerController {
 
-    private UsageRepository lesUsagerDeLaDB;
+    private UsagerRepository lesUsagerDeLaDB;
 
-    public UsagerController(UsageRepository ur) {
+    public UsagerController(UsagerRepository ur) {
         this.lesUsagerDeLaDB = ur;
 
         lesUsagerDeLaDB.save(new Usager());
--- src/main/java/fr/uha/serfa/lpdaoo25/BiblioTook/dao/UsagerRepository.java
+++ src/main/java/fr/uha/serfa/lpdaoo25/BiblioTook/dao/UsagerRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsageRepository extends JpaRepository<Usager, Long> {
+public interface UsagerRepository extends JpaRepository<Usager, Long> {
 
 
 
