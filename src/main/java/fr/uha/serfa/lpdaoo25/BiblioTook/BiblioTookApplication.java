28fcb9c33ab8ff9d31f3311d440aead81de506df
Florent.Bourgeois <florent.bourgeois@uha.fr>
2025-02-18 14:36:57 +0100

    ajout d'usager et chercher les emprunteurs

--- src/main/java/fr/uha/serfa/lpdaoo25/BiblioTook/BiblioTookApplication.java
+++ src/main/java/fr/uha/serfa/lpdaoo25/BiblioTook/BiblioTookApplication.java
@@ -1,15 +1,20 @@
 package fr.uha.serfa.lpdaoo25.BiblioTook;
 
 import fr.uha.serfa.lpdaoo25.BiblioTook.dao.AuteurRepository;
+import fr.uha.serfa.lpdaoo25.BiblioTook.dao.LivreRepository;
 import fr.uha.serfa.lpdaoo25.BiblioTook.dao.UsagerRepository;
 import fr.uha.serfa.lpdaoo25.BiblioTook.model.Bibliotheque;
+import fr.uha.serfa.lpdaoo25.BiblioTook.model.Livre;
+import fr.uha.serfa.lpdaoo25.BiblioTook.model.Usager;
 import fr.uha.serfa.lpdaoo25.BiblioTook.utils.BibliothequeFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sound.midi.Soundbank;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class BiblioTookApplication {
@@ -19,12 +24,30 @@ public class BiblioTookApplication {
 	}
 
 	@Bean
-	CommandLineRunner initRepos(AuteurRepository auteurRepo, UsagerRepository usagerRepo) {
+	CommandLineRunner initRepos(AuteurRepository auteurRepo, UsagerRepository usagerRepo, LivreRepository livreRepository) {
 		return args -> {
 			System.out.println("\n\nInitialisation du projet\n---------------");
 			Bibliotheque b = BibliothequeFactory.getBigBibliotheque();
 			System.out.println(b.tousLesAuteurs());
 			auteurRepo.saveAll(b.tousLesAuteurs());
+
+			Livre l1 = livreRepository.findById(1L).get();
+			Livre l2 = livreRepository.findById(2L).get();
+			Livre l3 = livreRepository.findById(3L).get();
+
+			Usager u1 = new Usager("Joe", "Joe",  LocalDate.of(1999,10,01));
+			Usager u2 = new Usager("Usager2", "prenom",  LocalDate.of(1999,10,01));
+			Usager u3 = new Usager("Usager3", "prenom3",  LocalDate.now());
+
+			u1.setEmprunt(l1);
+			u2.setEmprunt(l2);
+
+			usagerRepo.save(u1);
+			usagerRepo.save(u2);
+			usagerRepo.save(u3);
+
+			System.out.println(usagerRepo.findDistinctByEmpruntNotNull());
+
 			System.out.println("\n\nFIN \n---------------");
 		};
 	}
--- src/main/java/fr/uha/serfa/lpdaoo25/BiblioTook/dao/LivreRepository.java
+++ src/main/java/fr/uha/serfa/lpdaoo25/BiblioTook/dao/LivreRepository.java
@@ -1,15 +1,13 @@
 package fr.uha.serfa.lpdaoo25.BiblioTook.dao;
 
+import fr.uha.serfa.lpdaoo25.BiblioTook.model.Livre;
 import fr.uha.serfa.lpdaoo25.BiblioTook.model.Usager;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
 @Repository
-public interface UsagerRepository extends JpaRepository<Usager, Long> {
+public interface LivreRepository extends JpaRepository<Livre, Long> {
 
-
-    Set<Usager> findDistinctByEmpruntNotNull();
 }
